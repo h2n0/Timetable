f03fb1d4029ede568468975bf621a3e573a80e7c
h2n0 <elc954@hotmail.co.uk>
2017-10-21 14:32:12 +0100

    Making the install a little bit easier

---
+++ install.sh
@@ -0,0 +1,30 @@
+apt-get update > /dev/null
+apt-get uprade > /dev/null
+
+# Really, really need this one
+apt-get install ruby > /dev/null
+if [ $? != 0 ]; then
+	echo "Failed to install ruby"
+	exit 1
+fi
+
+# Needed for Nokogiri
+apt-get install build-essential patch ruby-dev zlib1g-dev liblzma-dev > /dev/null
+#
+if [ $? != 0 ]; then
+	echo "Failed to install Nokogiri essentals"
+	exit 1
+fi
+
+# Setup all of the gems needed to run the server
+gem install bundler
+if [ $? != 0 ]; then
+	echo "Failed to install bundler"
+	exit 1
+fi
+
+bundler install
+if [ $? != 0 ]; then
+	echo "Failed to install required gems"
+	exit 1
+fi
